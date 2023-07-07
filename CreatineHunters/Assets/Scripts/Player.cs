66675cf6d848231ce030723a0c3d12dd1bf3feef
JULIO MAXIMUS CAMPELO DO NASCIMENTO <20211170150065@ifrn.local>
2023-07-06 22:27:17 -0300

    muita coisa no player e cam
    lero

--- CreatineHunters/Assets/Scripts/Player.cs
+++ CreatineHunters/Assets/Scripts/Player.cs
@@ -9,6 +9,7 @@ public class Player : MonoBehaviour
     public float speed;
     public float jumpforce;
     private bool isJumping;
+    private bool isFire;
 
     private Rigidbody2D rig;
     private Animator anim;
@@ -25,26 +26,35 @@ public class Player : MonoBehaviour
     {
         Move();
         Jump();
+        RayFire();
     }
     void Move()
     {
         float movement = Input.GetAxis("Horizontal");
         rig.velocity = new Vector2(movement * speed, rig.velocity.y);
 
-        if (movement > 0) 
+        if (movement > 0 ) 
         {
+            if (!isJumping) 
+            {
+                anim.SetInteger("transition", 1);
+
+            }
             transform.eulerAngles = new Vector3(0,0,0);
-            anim.SetInteger("transition", 1);
 
         }
 
         if (movement < 0)
         {
+            if (!isJumping) 
+            {
+                anim.SetInteger("transition", 1);
+            }
             transform.eulerAngles = new Vector3(0, 180, 0);
-            anim.SetInteger("transition", 1);
+            
 
         }
-        if (movement == 0)
+        if (movement == 0 && !isJumping && !isFire)
             anim.SetInteger("transition", 0);   
 
     }
@@ -54,15 +64,34 @@ public class Player : MonoBehaviour
         {
             if (!isJumping)
             {
+                anim.SetInteger("transition", 2);
                 rig.AddForce(new Vector2(0, jumpforce), ForceMode2D.Impulse);
                 isJumping = true;
-                anim.SetInteger("transition", 2);
+               
 
             }
 
         }  
     }
 
+    void RayFire()
+    {
+
+        StartCoroutine("Fire");
+    }
+
+    
+    IEnumerator Fire()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            isFire = true;
+            anim.SetInteger("transition", 3);
+            yield return new WaitForSeconds(0.7f);
+            anim.SetInteger("transition", 0);
+
+        }
+    }
     private void OnCollisionEnter2D(Collision2D coll)
     {
         if(coll.gameObject.layer == 8)
